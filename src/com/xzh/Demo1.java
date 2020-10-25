7e89e5b8fc2f5eae78236850ccd54c3faa7e54f0
xzh <408796248@qq.com>
2020-10-25 17:53:09 +0800

    xzh

--- src/com/xzh/Demo1.java
+++ src/com/xzh/Demo1.java
@@ -1,12 +1,11 @@
 package com.xzh;
 
-import java.util.Date;
+import java.util.*;
 
 public class Demo1 {
     public static void main(String[] args) {
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 10; i++) {
             System.out.println(new Date());
-            
         }
     }
 }
